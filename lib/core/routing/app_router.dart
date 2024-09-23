11335e0369cdf3cd126e2dceb56ccab354f06e83
RawanHY <rawanhajyihya2000@gmail.com>
2024-09-23 14:00:06 +0300

    - added theming and color and style files

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'routes.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      // case Routes.onBoardingScreen:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const OnboardingScreen(),
+      //   );
+
+      default:
+        return null;
+    }
+  }
+}
---
+++ lib/sanad_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/app_router.dart';
+import 'core/routing/routes.dart';
+import 'core/theming/colors.dart';
+
+class SanadApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const SanadApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
