ca46d3ad38213ddeee57cc070b44a845e57e06a6
RawanHY <rawanhajyihya2000@gmail.com>
2024-09-24 09:38:36 +0300

    - added login feature

---
+++ lib/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(const LoginState.initial());
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  void emitLoginStates() async {
+    emit(const LoginState.loading());
+    // discuss another states
+  }
+}
